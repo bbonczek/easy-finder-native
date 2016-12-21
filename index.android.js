2866b1956b875ebcd8b934a2e6d88e757f746f2a
GRAMOFON\Michał <kapuchaster@gmail.com>
2016-12-21 21:54:53 +0100

    *login function test

--- index.android.js
+++ index.android.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { AppRegistry, View, Text } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import WelcomeScreen from './scripts/welcome_screen.js';
 
 export default class easyFinderNative extends Component {
     render() {
         return (
           <View>
-            <Text>easy-finder-native</Text>
             <WelcomeScreen />
           </View>
         );
--- scripts/welcome_screen.js
+++ scripts/welcome_screen.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react';
-import {Text, View, Image, Button, TextInput } from 'react-native';
+import { Text, View, Image, Button, TextInput} from 'react-native';
 
 export default class WelcomeScreen extends Component {
   constructor(props){
     super(props);
     this.padlock = require('../imgs/padlock.png');
     this.lang = require('../langs/ENG.json');
+    this.login = this.login.bind(this);
+    this.state = { loginInputText: '' };
+    this.state = { passwordInputText: '' };
+    this.state = { debugLog: 'myTestLog' };
   }
-    render() {
-        return (
-          <View>
-            <Text>{this.lang.welcome.login_example}</Text>
-            <Image source = {this.padlock} />
-            <TextInput placeholder = {this.lang.welcome.username} />
-            <TextInput placeholder = {this.lang.welcome.password} />
-            <Button title="Log In" />
-          </View>
-        );
+
+  login(){
+    let login = this.state.loginInputText;
+    let password = this.state.passwordInputText;
+
+    if(login == null || password == null){
+      this.setState({debugLog: "L |& P is: null"});
+      return;
     }
+
+    let myMsg = "L: " + login + " P: " + password;
+    this.setState({debugLog: myMsg});
+
+  }
+
+  render() {
+      return (
+        <View >
+          <Image source={this.padlock} />
+          <Text> {this.lang.welcome.login_example} </Text>
+          <TextInput
+            placeholder={this.lang.welcome.username}
+            onChangeText={(loginInputText) => this.setState({loginInputText})}
+            value={this.state.loginInputText}
+          />
+          <TextInput
+            placeholder={this.lang.welcome.password}
+            onChangeText={(passwordInputText) => this.setState({passwordInputText})}
+            value={this.state.passwordInputText}
+          />
+          <Button title="Log In" onPress={this.login}/>
+          <Text> {this.state.debugLog} </Text>
+        </View>
+      );
+  }
 }
