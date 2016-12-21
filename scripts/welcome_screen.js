f797f391e0119534fdfb19577c67fa5cdd98f623
GRAMOFON\Michał <kapuchaster@gmail.com>
2016-12-21 16:54:18 +0100

    *Added padlock image *Added all required components to the Welcome Screen (without CSS)

--- scripts/welcome_screen.js
+++ scripts/welcome_screen.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-import {Text, View } from 'react-native';
+import {Text, View, Image, Button, TextInput } from 'react-native';
 
 export default class WelcomeScreen extends Component {
+  constructor(props){
+    super(props);
+    this.padlock = require('../imgs/padlock.png');
+  }
     render() {
         return (
           <View>
-            <Text>Login Screen</Text>
+            <Text>Login Example</Text>
+            <Image source = {this.padlock} />
+            <TextInput placeholder="Username" />
+            <TextInput placeholder="Password" />
+            <Button title="Log In" />
           </View>
         );
     }
