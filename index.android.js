fd72b454ecebf91e38ef95ecdaa8f0840198400f
GRAMOFON\Michał <kapuchaster@gmail.com>
2016-12-21 16:41:33 +0100

    *View frames *Initialization of the Welcome Screen

--- index.android.js
+++ index.android.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry, View, Text } from 'react-native';
+import WelcomeScreen from './scripts/welcome_screen.js';
 
 export default class easyFinderNative extends Component {
     render() {
         return (
+          <View>
             <Text>easy-finder-native</Text>
+            <WelcomeScreen />
+          </View>
         );
     }
 }
